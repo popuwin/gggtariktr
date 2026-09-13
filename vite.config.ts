173f32e8dda2a5fa6c9348847da1e66b23b77c60
LLL112341 <dillarionov017@gmail.com>
2026-09-13 14:45:03 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,20 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+  import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/gggtariktr/",   // <-- ДОБАВЬ ЭТУ СТРОКУ
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
